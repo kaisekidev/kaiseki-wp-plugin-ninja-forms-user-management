3f07c3eb4f63ff8c89c03f82bc6defe78f5fc5ac
David Mondok <3883758+davidmondok@users.noreply.github.com>
2023-12-06 13:40:08 +0100

    update dependencies

--- src/FilterUserSettingsFactory.php
+++ src/FilterUserSettingsFactory.php
@@ -14,7 +14,7 @@ final class FilterUserSettingsFactory
     {
         $config = Config::get($container);
         /** @var array<mixed> $userSettings */
-        $userSettings = $config->array('ninja_forms_user_management/user_settings', []);
+        $userSettings = $config->array('ninja_forms_user_management/user_settings');
         return new FilterUserSettings(
             $container->get(NestedArray::class),
             $userSettings
